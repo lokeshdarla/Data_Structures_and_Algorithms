4738b2b367d1bf85de618e76a011b3b76463f356
lokeshdarla <lokeshnagasaidarla@gmail.com>
2025-02-24 23:31:43 +0530

    [Arrays] Count Pairs

--- 04_Arrays/CountPairs.java
+++ 04_Arrays/CountPairs.java
@@ -1,3 +1,23 @@
 public class CountPairs {
 
+    public static int countPairs(char[] arr) {
+        int n = arr.length;
+        int cg = 0;
+        int p = 0;
+
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] == 'g') {
+                cg++;
+            } else if (arr[i] == 'a') {
+                p += cg;
+            }
+        }
+        return p;
+    }
+
+    public static void main(String[] args) {
+        char[] arr = {'a', 'g', 'a', 'g', 'g'};
+        System.out.println("Count of pairs: " + countPairs(arr));
+    }
 }
